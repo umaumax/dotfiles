943548365c5bee019c97d1f501cba5de1727a2c7
umaumax <umauma7083@gmail.com>
2018-08-28 23:09:43 +0900

    Update cmake snippets

--- template/cmake/base-main.cmake
+++ template/cmake/base-main.cmake
@@ -1,17 +1,20 @@
-cmake_minimum_required (VERSION 2.8)
-project (CMakeHelloWorld)
+cmake_minimum_required(VERSION 2.8)
+project(CMakeHelloWorld)
 
 #version number
-set (CMakeHelloWorld_VERSION_MAJOR 1)
-set (CMakeHelloWorld_VERSION_MINOR 0)
+set(CMakeHelloWorld_VERSION_MAJOR 1)
+set(CMakeHelloWorld_VERSION_MINOR 0)
 
 #include the subdirectory containing our libs
-add_subdirectory (Hello)
+add_subdirectory(Hello)
+
+add_definitions(-DFOO)
 include_directories(Hello)
+link_directories(/path1/to/lib /path2/to/lib)
 #indicate the entry point for the executable
-add_executable (CMakeHelloWorld Hello HelloWorld.cpp)
+add_executable(CMakeHelloWorld Hello HelloWorld.cpp)
 
 # Indicate which libraries to include during the link process.
-target_link_libraries (CMakeHelloWorld Hello)
+target_link_libraries(CMakeHelloWorld Hello)
 
-install (TARGETS CMakeHelloWorld DESTINATION bin)
+install(TARGETS CMakeHelloWorld DESTINATION bin)
