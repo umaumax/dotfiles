b4a72a27120a2f8cd27d7b6dc0fd8681811679f2
umaumax <umauma7083@gmail.com>
2018-09-10 22:04:30 +0900

    Add screenrc setting

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@
 set -x
 ln -sf ~/dotfiles/.vimrc ~/
 ln -sf ~/dotfiles/.tmux.conf ~/
+ln -sf ~/dotfiles/.screenrc ~/
 ln -sf ~/dotfiles/.tigrc ~/
 ln -sf ~/dotfiles/.clang-format ~/
 ln -sf ~/dotfiles/.zshrc ~/
