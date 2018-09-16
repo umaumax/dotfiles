23272dd8baa960a0eed211b87d16e95ec2623dcb
umaumax <umauma7083@gmail.com>
2018-09-17 02:49:20 +0900

    Update snippets

--- scripts/setup/go.sh
+++ scripts/setup/go.sh
@@ -4,4 +4,8 @@ set -e
 go get -u github.com/mattn/files
 go get -u mvdan.cc/sh/cmd/shfmt
 
+# FYI: [ターミナルで簡単にグラフを描くツール termeter \- Qiita]( https://qiita.com/atsaki/items/e7d2e53bac8ba6fdbce0 )
+go get -u github.com/atsaki/termeter/cmd/termeter
+
+# my tools
 go get -u github.com/umaumax/golfix
