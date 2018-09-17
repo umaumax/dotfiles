45b8c20c6f2f800b5a25eef7d711f00c24be1a0a
umaumax <umauma7083@gmail.com>
2018-09-17 22:59:20 +0900

    Add linuxbrew setup script

---
+++ scripts/setup/linuxbrew.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# LinuxBrew installs software in user-specified directory (not system-wide), and does not require sudo access.
+
+# maybe latest(so I recommend to install below package by linuxbrew or build your self)
+brew install git    # 2.19.0
+brew install tig    # 2.4.1
+brew install peco   # 0.5.3
+brew install fzy    # 0.9
+brew install fzf    # 0.17.4
+brew install tmux   # 2.7
+brew install screen # 4.06.02
+brew install go     # 1.11
+
+brew install wget
+brew install tree
+brew install lsof curl
+brew install readline cmake jq
+brew install ncurses openssl
+brew install pkg-config
+brew install ccat
+brew install ninja
+
+# takes a lot of time
+brew install vim # 8.1.0350 :requires: python: this takes a lot of time
+brew install python
+
+# takes a loooooot of time
+brew install bat # x.x.x :requires: rust: this takes a lot of time
+brew install rust
+
+brew install clang-format
+
+# brew install neovim # error
+brew install umaumax/neovim/neovim
