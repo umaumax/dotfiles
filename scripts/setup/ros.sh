b59902420ea68f9d4b4b3daa59ba05f8cf57ae68
umaumax <umauma7083@gmail.com>
2018-09-30 17:49:23 +0900

    Add ubuntu16.04 kinetic ros setup script

---
+++ scripts/setup/ros.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# FYI: [kinetic/Installation/Ubuntu \- ROS Wiki]( http://wiki.ros.org/kinetic/Installation/Ubuntu )
+
+# ubuntu16.04: kinetic
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install -y ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+
+# echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+# source ~/.bashrc
+# # or
+# echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
+# source ~/.zshrc
+
+# NOTE: Dependencies for building packages
+sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
