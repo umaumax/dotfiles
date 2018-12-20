11147e16f9c33a57089ddd20124807a4356ac19f
umaumax <umauma7083@gmail.com>
2018-12-20 23:14:35 +0900

    Update setup go script

--- scripts/setup/go.sh
+++ scripts/setup/go.sh
@@ -11,6 +11,7 @@ go get -u github.com/zmb3/gogetdoc
 go get -u mvdan.cc/sh/cmd/shfmt
 # FYI: [ターミナルで簡単にグラフを描くツール termeter \- Qiita]( https://qiita.com/atsaki/items/e7d2e53bac8ba6fdbce0 )
 go get -u github.com/atsaki/termeter/cmd/termeter
+go get -u github.com/mattn/typogrep
 
 # my tools
 go get -u github.com/umaumax/gechota
