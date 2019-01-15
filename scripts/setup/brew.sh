389c9d2cb4943a006f93db82f9541f03414fa67f
umaumax <umauma7083@gmail.com>
2019-01-15 23:50:25 +0900

    Add progress command brew setup script

--- scripts/setup/brew.sh
+++ scripts/setup/brew.sh
@@ -9,6 +9,10 @@ brew install colormake
 # for c++ lib
 brew install google-benchmark
 
+# for confirm cp command progress
+# [Xfennec/progress: Linux tool to show progress for cp, mv, dd, \.\.\. \(formerly known as cv\)]( https://github.com/Xfennec/progress )
+brew install progress
+
 # brew tap
 # WIP
 
