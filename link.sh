aa11568135ac40fb7a4b57932889a7ea119259d0
umaumax <umauma7083@gmail.com>
2019-02-02 12:00:08 +0900

    Update link script

--- link.sh
+++ link.sh
@@ -1,112 +1,109 @@
 #!/usr/bin/env bash
+current_abs_directory_path=$(cd $(dirname $0) && pwd)
+cd ${current_abs_directory_path%/}
 
-set -x
+set -ex
 
 # git submodule
 git submodule update -i
 
 # link
+dotfiles=(
+	.vimrc
+	.tmux.conf
+	.screenrc
+	.tigrc
+	.clang-format
+	.zshrc
+	.zprofile
+	.zshenv
+	.zpreztorc
+	.textlintrc
+	.inputrc
+	.gdbinit
+	.gdb-dashboard
+	.wgit
+)
+for filepath in "${dotfiles[@]}"; do
+	echo ln -sf ~/dotfiles/"$filepath" ~/
+done
 
-ln -sf ~/dotfiles/.vimrc ~/
-ln -sf ~/dotfiles/.tmux.conf ~/
-ln -sf ~/dotfiles/.screenrc ~/
-ln -sf ~/dotfiles/.tigrc ~/
-ln -sf ~/dotfiles/.clang-format ~/
-ln -sf ~/dotfiles/.zshrc ~/
-ln -sf ~/dotfiles/.zprofile ~/
-ln -sf ~/dotfiles/.zshenv ~/
-ln -sf ~/dotfiles/.zpreztorc ~/
-ln -sf ~/dotfiles/.textlintrc ~/
-ln -sf ~/dotfiles/.inputrc ~/
-
-ln -sf ~/dotfiles/.gdbinit ~/
-ln -sf ~/dotfiles/.gdb-dashboard ~/
-
+zsh_dotfiles=(
+	.git.zshrc
+	.peco.zshrc
+	.zplug.zshrc
+	.comp.zshrc
+	.bindkey.zshrc
+	.windows.zshrc
+	.nugget.zshrc
+	.ros.zshrc
+	.prezto.git.init.zshrc
+)
 [[ ! -d ~/.zsh/ ]] && mkdir -p ~/.zsh
-ln -sf ~/dotfiles/.git.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.peco.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.zplug.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.comp.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.bindkey.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.windows.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.nugget.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.ros.zshrc ~/.zsh/
-ln -sf ~/dotfiles/.prezto.git.init.zshrc ~/.zsh/
+for filepath in "${zsh_dotfiles[@]}"; do
+	echo ln -sf ~/dotfiles/"$filepath" ~/.zsh/
+done
 
 # cp not ln -f
 [[ ! -f ~/.gitconfig ]] && cp ~/dotfiles/.tmpl.gitconfig ~/.gitconfig
 [[ ! -f ~/.local.vimrc ]] && cp ~/dotfiles/.tmpl.local.vimrc ~/.local.vimrc
-
-[[ ! -d ~/.local.git_template/ ]] && command cp -r ~/dotfiles/.local.git_template ~/
+[[ ! -d ~/.local.git_template/ ]] && cp -R ~/dotfiles/.local.git_template ~/
 
 # cp .git_template dirs
-(cd ~/dotfiles && find .git_template/hooks -type d -not -name '.*' | xargs -L 1 -IXXX mkdir -p "$HOME/XXX")
+find .git_template/hooks -type d -not -name '.*' -print0 | xargs -0 -L 1 -IXXX mkdir -p "$HOME/XXX"
 # ln .git_template files
-(cd ~/dotfiles && find .git_template/hooks -type f -not -name '.*' | xargs -L 1 -IXXX ln -sf "$HOME/dotfiles/XXX" "$HOME/XXX")
+find .git_template/hooks -type f -not -name '.*' -print0 | xargs -0 -L 1 -IXXX ln -sf "$HOME/dotfiles/XXX" "$HOME/XXX"
 # -a: enable copy symbolic links
-(cd ~/dotfiles && find .git_template/hooks -type l -not -name '.*' | xargs -L 1 -IXXX cp -a "$HOME/dotfiles/XXX" "$HOME/XXX")
-
-ln -sf ~/dotfiles/.wgit ~/
+find .git_template/hooks -type l -not -name '.*' -print0 | xargs -0 -L 1 -IXXX cp -a "$HOME/dotfiles/XXX" "$HOME/XXX"
 
 [[ ! -d ~/.vim/ ]] && mkdir -p ~/.vim
 # NOTE: windows ln behave like cp (to avoid 'cannot overwrite directory')
-if [[ "$OS" =~ "Windows" ]]; then
+if [[ $OS =~ Windows ]]; then
 	[[ -e ~/.vim/config ]] && rm -rf ~/.vim/config
 fi
 ln -fs ~/dotfiles/.vim/config ~/.vim/
 
-[[ ! -d ~/.config/ ]] && mkdir -p ~/.config
-[[ ! -d ~/.config/peco ]] && mkdir -p ~/.config/peco
-ln -sf ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
-
-[[ ! -d ~/.config/git ]] && mkdir -p ~/.config/git
-ln -sf ~/dotfiles/.config/git/ignore ~/.config/git/ignore
-ln -sf ~/dotfiles/.config/git/ignore ~/.gitignore
-
-[[ ! -d ~/.config/golfix ]] && mkdir -p ~/.config/golfix
-ln -sf ~/dotfiles/.config/golfix/*.golfix ~/.config/golfix/
-
-[[ ! -d ~/.config/nvim ]] && mkdir -p ~/.config/nvim
-# ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/
-ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
-
-if [[ $(uname) == "Linux" ]]; then
-	ln -sf ~/dotfiles/.xbindkeysrc ~/
-	[[ ! -d ~/.config/xkeysnail ]] && mkdir -p ~/.config/xkeysnail
-	ln -sf ~/dotfiles/.config/xkeysnail/config.py ~/.config/xkeysnail/config.py
-	ln -sf ~/dotfiles/.toprc ~/
-
-	[[ ! -d ~/.config/autostart ]] && mkdir -p ~/.config/autostart
-	(cd ~/dotfiles/.config/autostart && find . -type f -name '*.desktop' | xargs -L 1 -IXXX ln -sf "$HOME/dotfiles/.config/autostart/XXX" "$HOME/.config/autostart/XXX")
-
-	[[ ! -d ~/.config/tilda ]] && mkdir -p ~/.config/tilda
-	ln -sf ~/dotfiles/.config/tilda/style.css ~/.config/tilda/style.css
-fi
-
-[[ ! -d ~/.config/alacritty ]] && mkdir -p ~/.config/alacritty
-# backup original setting file
-[[ -f ~/.config/alacritty/alacritty.yml ]] && [[ ! -L ~/.config/alacritty/alacritty.yml ]] && mv ~/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml~
-ln -sf ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-
 [[ -z $XDG_CONFIG_HOME ]] && XDG_CONFIG_HOME=".config"
 ln -sf ~/dotfiles/.config/pep8 "$HOME/$XDG_CONFIG_HOME/pep8"
 
-[[ ! -d ~/.config/oressh ]] && mkdir -p ~/.config/oressh/default/
-ln -sf ~/dotfiles/.minimal.bashrc ~/.config/oressh/default/.bashrc
-ln -sf ~/dotfiles/.minimal.vimrc ~/.config/oressh/default/.vimrc
-ln -sf ~/dotfiles/.inputrc ~/.config/oressh/default/.inputrc
+# NOTE: auto create .config dir
+find .config -type d -print0 | xargs -0 -L 1 -IXXX mkdir -p "$HOME/XXX"
 
-if [[ -d ~/.config/karabiner/assets/complex_modifications ]]; then
-	while read line || [ -n "${line}" ]; do
-		filepath=$line
-		dirname=${filepath%/*}
-		basename=${filepath##*/}
-		karabiner_name=${filepath#*.}
-		ln -sf $PWD/$filepath ~/.config/karabiner/assets/complex_modifications/$karabiner_name
-	done < <(ls karabiner/*.json)
+ln -sf ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
+ln -sf ~/dotfiles/.config/git/ignore ~/.gitignore
+ln -sf ~/dotfiles/.config/git/ignore ~/.config/git/ignore
+ln -sf ~/dotfiles/.config/golfix/*.golfix ~/.config/golfix/
+ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+
+if [[ $(uname) == "Linux" ]]; then
+	ln -sf ~/dotfiles/.toprc ~/
+	ln -sf ~/dotfiles/.xbindkeysrc ~/
+	ln -sf ~/dotfiles/.config/xkeysnail/config.py ~/.config/xkeysnail/config.py
+	ln -sf ~/dotfiles/.config/autostart/*.desktop ~/.config/autostart/
+	ln -sf ~/dotfiles/.config/tilda/style.css ~/.config/tilda/style.css
 fi
 
-# if [[ -n $LOCAL_BIN ]]; then
-# 	[[ ! -d ~/local/bin ]] && mkdir -p ~/local/bin
-# 	ln -sf ~/dotfiles/local/bin/xxx ~/local/bin/xxx
-# fi
+# NOTE: backup original setting file if exists
+function safe_ln() {
+	[[ $# -lt 2 ]] && echo "$(basename "$0") source_file target_file" && return 1
+	local source_file=$1
+	local target_file=$2
+	# NOTE: backup
+	[[ -f "$target_file" ]] && [[ ! -L "$target_file" ]] && mv "$target_file" "${target_file}~"
+	# NOTE: target_file -> source_file
+	ln -sf "$source_file" "$target_file"
+}
+safe_ln ~/dotfiles/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+safe_ln ~/dotfiles/.minimal.bashrc ~/.config/oressh/default/.bashrc
+safe_ln ~/dotfiles/.minimal.vimrc ~/.config/oressh/default/.vimrc
+safe_ln ~/dotfiles/.inputrc ~/.config/oressh/default/.inputrc
+
+if [[ -d ~/.config/karabiner/assets/complex_modifications ]]; then
+	ls karabiner/*.json | while read -r line || [ -n "${line}" ]; do
+		filepath=$line
+		karabiner_name=${filepath#*.}
+		ln -sf ~/dotfiles/$filepath ~/.config/karabiner/assets/complex_modifications/$karabiner_name
+	done
+fi
+
+echo "[OK]"
