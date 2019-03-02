fbfe0fd152194e4040e66a5dd1761f53d7cee7eb
umaumax <umauma7083@gmail.com>
2019-03-03 02:29:29 +0900

    Update shell color snip template

--- template/sh/snip-color.sh
+++ template/sh/snip-color.sh
@@ -1,2 +1,1 @@
-BLACK="\033[0;30m" RED="\033[0;31m" GREEN="\033[0;32m" YELLOW="\033[0;33m" BLUE="\033[0;34m" PURPLE="\033[0;35m" LIGHT_BLUE="\033[0;36m" WHITE="\033[0;37m" GRAY="\033[0;39m" DEFAULT="\033[0m"
-function echo() { command echo -e "$@"; }
+BLACK=$'\e[0;30m' RED=$'\e[0;31m' GREEN=$'\e[0;32m' YELLOW=$'\e[0;33m' BLUE=$'\e[0;34m' PURPLE=$'\e[0;35m' LIGHT_BLUE=$'\e[0;36m' WHITE=$'\e[0;37m' GRAY=$'\e[0;90m' DEFAULT=$'\e[0m'
