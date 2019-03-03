6226f05b164a3c3f05a2f5a4fb52840541b9f440
umaumax <umauma7083@gmail.com>
2019-03-04 06:28:29 +0900

    Remove color env var 0;

--- template/sh/snip-color.sh
+++ template/sh/snip-color.sh
@@ -1,1 +1,1 @@
-BLACK=$'\e[0;30m' RED=$'\e[0;31m' GREEN=$'\e[0;32m' YELLOW=$'\e[0;33m' BLUE=$'\e[0;34m' PURPLE=$'\e[0;35m' LIGHT_BLUE=$'\e[0;36m' WHITE=$'\e[0;37m' GRAY=$'\e[0;90m' DEFAULT=$'\e[0m'
+BLACK=$'\e[30m' RED=$'\e[31m' GREEN=$'\e[32m' YELLOW=$'\e[33m' BLUE=$'\e[34m' PURPLE=$'\e[35m' LIGHT_BLUE=$'\e[36m' WHITE=$'\e[37m' GRAY=$'\e[90m' DEFAULT=$'\e[0m'
