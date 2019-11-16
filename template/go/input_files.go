47a30e62367f3657aba6646278892315efab178c
umaumax <umauma7083@gmail.com>
2019-11-17 01:32:38 +0900

    Fix vim golang template

--- template/go/input_files.go
+++ template/go/input_files.go
@@ -1,14 +1,15 @@
 // NOTE: default input file is input pipe
-var inputFiles []string
-if flag.NArg() == 0 {
-  inputFiles = append(inputFiles, os.Stdin.Name())
-} else {
-  inputFiles = append(inputFiles, flag.Args()...)
+inputFiles := []string{"-"}
+if flag.NArg() > 0 {
+	inputFiles = flag.Args()
 }
 for _, inputFile := range inputFiles {
-  file, err := os.Open(inputFile)
-  if err != nil {
-    log.Fatal("file read error:", err)
-  }
-  defer file.Close()
+	file := os.Stdin
+	if inputFile != "-" {
+		file, err := os.Open(inputFile)
+		if err != nil {
+			log.Fatal("file read error:", err)
+		}
+		defer file.Close()
+	}
 }
