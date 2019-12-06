adb90fcac4413508d95f05879c44404faca874a8
umaumax <umauma7083@gmail.com>
2019-12-06 23:41:28 +0900

    Update cargo script

--- scripts/setup/cargo.sh
+++ scripts/setup/cargo.sh
@@ -3,3 +3,10 @@ set -eux
 
 # NOTE: A command-line hex viewer
 cargo install hexyl
+
+# NOTE: rust repl
+# command: evcxr
+cargo install evcxr_repl
+
+# NOTE: for vim deoplete-rust
+cargo +nightly install racer
