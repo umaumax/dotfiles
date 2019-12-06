224e8a8e47d5c81e5b0526890bb4dcf7cb0cc60e
umaumax <umauma7083@gmail.com>
2019-12-07 03:33:52 +0900

    Update neosnippet lint script

--- neosnippet/neosnippet_lint.sh
+++ neosnippet/neosnippet_lint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
 current_abs_directory_path=$(cd $(dirname $0) && pwd)
 cd ${current_abs_directory_path%/}
 
+BLACK=$'\e[30m' RED=$'\e[31m' GREEN=$'\e[32m' YELLOW=$'\e[33m' BLUE=$'\e[34m' PURPLE=$'\e[35m' LIGHT_BLUE=$'\e[36m' WHITE=$'\e[37m' GRAY=$'\e[90m' DEFAULT=$'\e[0m'
+
 git grep --color=always -e '^snippet' -e '$0' -e '${0' -- '*.snip' | awk '$1 ~ /snippet/{if(snippet!=""){print snippet; }snippet=$0} $1 !~ /snippet/{snippet=""}'
+if [[ $? == 0 ]]; then
+  echo "${GREEN}[OK]${DEFAULT}"
+fi
