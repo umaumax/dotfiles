8c7606f58523220e0d021e808eb616a38102c488
umaumax <umauma7083@gmail.com>
2020-05-24 16:49:15 +0900

    Add gdb script update script

---
+++ .config/gdb/update.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git wget https://github.com/cyrus-and/gdb-dashboard/blob/master/.gdbinit -O .gdb-dashboard
+git wget https://github.com/sharkdp/stack-inspector/blob/master/stack-inspector.py
+git wget https://github.com/umaumax/gdb-hook/blob/master/gdb-trace.py
