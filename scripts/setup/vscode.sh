4dcea343f33156640cf10724029d34e79374b792
umaumax <umauma7083@gmail.com>
2020-12-13 23:37:18 +0900

    Update vscode setup script

--- scripts/setup/vscode.sh
+++ scripts/setup/vscode.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-set -ex
+set -eu
 
-code --install-extension jebbs.plantuml
-code --install-extension shd101wyy.markdown-preview-enhanced
+# how to list installed extensions
+# code --list-extensions
+
+extensions=(
+  jebbs.plantuml
+  shd101wyy.markdown-preview-enhanced
+  artdiniz.quitcontrol-vscode
+  GitHub.vscode-pull-request-github
+  jebbs.plantuml
+
+  # for unity
+  ms-dotnettools.csharp
+  ms-vscode.mono-debug
+  k--kato.docomment
+  Unity.unity-debug
+  zrachod.mono-snippets
+)
+for extension in "${extensions[@]}"; do
+  echo $'\e[92m'"$extension"$'\e[m'
+  code --install-extension "$extension"
+done
