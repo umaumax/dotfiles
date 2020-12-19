a7355fceb01f7df150f2f4cff432245fca344d8f
umaumax <umauma7083@gmail.com>
2020-12-19 13:25:11 +0900

    Fix gdb update script

--- .config/gdb/update.sh
+++ .config/gdb/update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+current_abs_directory_path=$(cd $(dirname $0) && pwd)
+cd ${current_abs_directory_path%/}
+
 git wget https://github.com/cyrus-and/gdb-dashboard/blob/master/.gdbinit -O .gdb-dashboard
 git wget https://github.com/sharkdp/stack-inspector/blob/master/stack-inspector.py
 git wget https://github.com/umaumax/gdb-hook/blob/master/gdb-trace.py
