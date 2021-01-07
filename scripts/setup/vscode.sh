48034b98c1ff59a72730f9115ba1cda446388ba0
umaumax <umauma7083@gmail.com>
2021-01-07 20:29:15 +0900

    Update vscode extension install command

--- scripts/setup/vscode.sh
+++ scripts/setup/vscode.sh
@@ -5,43 +5,58 @@ set -eu
 # how to list installed extensions
 # code --list-extensions
 
-extensions=(
-  jebbs.plantuml
-  shd101wyy.markdown-preview-enhanced
-  artdiniz.quitcontrol-vscode
-  GitHub.vscode-pull-request-github
-  jebbs.plantuml
-  wayou.vscode-todo-highlight
-  shardulm94.trailing-spaces
-  mosapride.zenkaku
-  YoshinoriN.current-file-path
-  ms-vscode-remote.vscode-remote-extensionpack
-  CoenraadS.bracket-pair-colorizer
-  ionutvmi.path-autocomplete
-  bwildeman.tabulous
-  sygene.auto-correct
-  oderwat.indent-rainbow
-  Atishay-Jain.All-Autocomplete
-  christian-kohler.path-intellisense
-  mhutchie.git-graph
-  praveencrony.total-lines
-  eamodio.gitlens
-  tranhl.find-then-jump
-  rust-lang.rust
-  zhengxiaoyao0716.intelligence-change-case
-  be5invis.toml
-  ZakCodes.rust-snippets
-  ryu1kn.text-marker
+remote_option=0
+if [[ $1 == "--remote" ]]; then
+  remote_option=1
+  echo $'\e[95m'"[ENABLE REMOTE OPTION]"$'\e[m'
+  shift
+fi
 
-  inu1255.easy-snippet
-  # for unity
-  ms-dotnettools.csharp
-  ms-vscode.mono-debug
-  k--kato.docomment
-  Unity.unity-debug
-  zrachod.mono-snippets
+extensions=(
+  Atishay-Jain.All-Autocomplete
+  CoenraadS.bracket-pair-colorizer
+  GitHub.vscode-pull-request-github
+  YoshinoriN.current-file-path
+  ZakCodes.rust-snippets
+  artdiniz.quitcontrol-vscode
+  be5invis.toml
+  bwildeman.tabulous
+  christian-kohler.path-intellisense
+  eamodio.gitlens
+  ionutvmi.path-autocomplete
+  jebbs.plantuml
+  mhutchie.git-graph
+  mosapride.zenkaku
+  oderwat.indent-rainbow
+  rust-lang.rust
+  ryu1kn.text-marker
+  shardulm94.trailing-spaces
+  shd101wyy.markdown-preview-enhanced
+  sygene.auto-correct
+  tranhl.find-then-jump
+  wayou.vscode-todo-highlight
+  zhengxiaoyao0716.intelligence-change-case
 )
+
+if [[ $(uname) == "Darwin" ]]; then
+  extensions=("${extensions[@]}"
+    ms-vscode-remote.vscode-remote-extensionpack
+    praveencrony.total-lines
+
+    # for unity
+    ms-dotnettools.csharp
+    ms-vscode.mono-debug
+    k--kato.docomment
+    Unity.unity-debug
+    zrachod.mono-snippets
+  )
+fi
+
 for extension in "${extensions[@]}"; do
   echo $'\e[92m'"$extension"$'\e[m'
-  code --install-extension "$extension"
+  if [[ "$remote_option" == 0 ]]; then
+    code --install-extension "$extension"
+  else
+    code --extensions-dir ~/.vscode-server/extensions --install-extension "$extension"
+  fi
 done
