5d7a528b1676de11ab57512523033754df14d032
umaumax <umauma7083@gmail.com>
2021-07-22 16:13:37 +0900

    Disable apt-get-ppa script

--- scripts/setup/apt-get-ppa.sh
+++ scripts/setup/apt-get-ppa.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
+# only for Ubuntu16.04
 # for vim 8.1
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo apt update
-sudo apt install -y vim
-sudo add-apt-repository -y --remove ppa:jonathonf/vim
+# sudo add-apt-repository -y ppa:jonathonf/vim
+# sudo apt update
+# sudo apt install -y vim
+# sudo add-apt-repository -y --remove ppa:jonathonf/vim
