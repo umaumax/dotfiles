ab3bf9ba57bdc5946e0330248278b051b8b7d99c
umaumax <umauma7083@gmail.com>
2021-10-16 17:52:47 +0900

    Update vim templates

--- template/cpp/base-main-assert.cpp
+++
@@ -1,5 +0,0 @@
-#include <cassert>
-int main(int argc, char* argv[]) {
-  {{_cursor_}}
-  return 0;
-}
--- template/cpp/base-main-stdio.cpp
+++ template/cpp/base-main-stdio.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+
 int main(int argc, char* argv[]) {
   std::printf("Hello world!\n");
   {{_cursor_}}
