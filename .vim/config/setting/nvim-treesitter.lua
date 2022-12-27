def6ae6ce2a495ba292b43007e64df9aa15591e4
umaumax <umauma7083@gmail.com>
2022-12-28 08:42:11 +0900

    Update nvim-treesitter setting

--- .vim/config/setting/nvim-treesitter.lua
+++ .vim/config/setting/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-	ignore_install = { "php", "tlaplus", "julia", "scheme" }, -- List of parsers to ignore installing
+	ignore_install = { "php", "phpdoc", "tlaplus", "julia", "scheme" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = {}, -- list of language that will be disabled
