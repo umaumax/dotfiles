7aff40e6b3f3183b26fb4ccc134ee53ed62a666d
umaumax <umauma7083@gmail.com>
2023-10-15 09:15:06 +0900

    Update Google Japanese Input Settings

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,4 @@
+-- window position and size shortcut
 hs.window.animationDuration = 0
 units = {
   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
@@ -21,3 +22,44 @@ hs.hotkey.bind(mash, '2',     function() hs.window.focusedWindow():move(units.up
 hs.hotkey.bind(mash, '3',     function() hs.window.focusedWindow():move(units.botleft50,  nil, true) end)
 hs.hotkey.bind(mash, '4',     function() hs.window.focusedWindow():move(units.botright50, nil, true) end)
 hs.hotkey.bind(mash, 'f',     function() hs.window.focusedWindow():move(units.maximum,    nil, true) end)
+
+-- [Hammerspoon で英数・かなの切り替えを行う]( https://zenn.dev/ytk6565/articles/hammerspoon-switch-input-source )
+-- commandwindow position and size shortcut
+local map = hs.keycodes.map
+local keyDown = hs.eventtap.event.types.keyDown
+local flagsChanged = hs.eventtap.event.types.flagsChanged
+local keyStroke = hs.eventtap.keyStroke
+
+local isCmdAsModifier = false
+
+local ESCAPE = 53 --hs.keycodes.map['escape']
+
+local function switchInputSourceEvent(event)
+    local eventType = event:getType()
+    local keyCode = event:getKeyCode()
+    local flags = event:getFlags()
+    local isCmd = flags['cmd']
+
+    if eventType == keyDown then
+        if isCmd then
+            isCmdAsModifier = true
+        end
+    elseif eventType == flagsChanged then
+        if not isCmd then
+            if isCmdAsModifier == false then
+                if keyCode == map['cmd'] then
+                    keyStroke({"shift"}, ESCAPE, 0) -- Replase to half ascii (Google Japanese Input at Compositio mode)
+                    keyStroke({"ctrl"}, ESCAPE, 0) --  Commit (Google Japanese Input at Compositio mode)
+                    -- keyStroke({}, ESCAPE, 0)
+                    keyStroke({}, 0x66, 0) -- 英数キー
+                elseif keyCode == map['rightcmd'] then
+                    keyStroke({}, 0x68, 0) -- かなキー
+                end
+            end
+            isCmdAsModifier = false
+        end
+    end
+end
+
+eventTap = hs.eventtap.new({keyDown, flagsChanged}, switchInputSourceEvent)
+eventTap:start()
