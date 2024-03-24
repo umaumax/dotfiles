93975d21148bc708bc5155ab126a04d4a983be46
umaumax <umauma7083@gmail.com>
2024-03-24 19:08:42 +0900

    Update indent-blackline plugin

--- .vim/config/setting-end/setting.lua
+++ .vim/config/setting-end/setting.lua
@@ -37,57 +37,90 @@ require("fine-cmdline").setup({
 })
 
 -- lukas-reineke/indent-blankline.nvim
+
+indent_blankline_version = 3
+
 vim.opt.list = true
 vim.opt.listchars:append("space: ")
 vim.opt.listchars:append("eol:↲")
 
 vim.opt.termguicolors = true
-vim.cmd([[highlight IndentBlanklineIndent1 guifg=#703C35 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent2 guifg=#75603B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent3 guifg=#486339 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent4 guifg=#265662 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent5 guifg=#315F7F gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineIndent6 guifg=#66386D gui=nocombine]])
 
-vim.cmd([[highlight IndentBlanklineContextIndent1 guifg=#E06C75 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextIndent2 guifg=#E5C07B gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextIndent3 guifg=#98C379 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextIndent4 guifg=#56B6C2 gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextIndent5 guifg=#61AFEF gui=nocombine]])
-vim.cmd([[highlight IndentBlanklineContextIndent6 guifg=#C678DD gui=nocombine]])
+-- version 3
+if indent_blankline_version == 3 then
+	local highlight = {
+		"RainbowRed",
+		"RainbowYellow",
+		"RainbowBlue",
+		"RainbowOrange",
+		"RainbowGreen",
+		"RainbowViolet",
+		"RainbowCyan",
+	}
 
-require("indent_blankline").setup({
-	char = "|",
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
-	char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-		"IndentBlanklineIndent5",
-		"IndentBlanklineIndent6",
-	},
-	context_patterns = {
-		"class",
-		"function",
-		"method",
-		"if",
-		"for",
-		"loop",
-		"let",
-		"while",
-	},
-	context_highlight_list = {
-		"IndentBlanklineContextIndent1",
-		"IndentBlanklineContextIndent2",
-		"IndentBlanklineContextIndent3",
-		"IndentBlanklineContextIndent4",
-		"IndentBlanklineContextIndent5",
-		"IndentBlanklineContextIndent6",
-	},
-})
+	local hooks = require("ibl.hooks")
+	-- create the highlight groups in the highlight setup hook, so they are reset
+	-- every time the colorscheme changes
+	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	end)
+
+	require("ibl").setup({ indent = { char = "|", highlight = highlight } })
+else
+	-- version 2
+	vim.cmd([[highlight IndentBlanklineIndent1 guifg=#703C35 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineIndent2 guifg=#75603B gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineIndent3 guifg=#486339 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineIndent4 guifg=#265662 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineIndent5 guifg=#315F7F gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineIndent6 guifg=#66386D gui=nocombine]])
+
+	vim.cmd([[highlight IndentBlanklineContextIndent1 guifg=#E06C75 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextIndent2 guifg=#E5C07B gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextIndent3 guifg=#98C379 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextIndent4 guifg=#56B6C2 gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextIndent5 guifg=#61AFEF gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextIndent6 guifg=#C678DD gui=nocombine]])
+
+	require("indent_blankline").setup({
+		char = "|",
+		space_char_blankline = " ",
+		show_current_context = true,
+		show_current_context_start = true,
+		char_highlight_list = {
+			"IndentBlanklineIndent1",
+			"IndentBlanklineIndent2",
+			"IndentBlanklineIndent3",
+			"IndentBlanklineIndent4",
+			"IndentBlanklineIndent5",
+			"IndentBlanklineIndent6",
+		},
+		context_patterns = {
+			"class",
+			"function",
+			"method",
+			"if",
+			"for",
+			"loop",
+			"let",
+			"while",
+		},
+		context_highlight_list = {
+			"IndentBlanklineContextIndent1",
+			"IndentBlanklineContextIndent2",
+			"IndentBlanklineContextIndent3",
+			"IndentBlanklineContextIndent4",
+			"IndentBlanklineContextIndent5",
+			"IndentBlanklineContextIndent6",
+		},
+	})
+end
 
 require("colorizer").setup({
 	"html",
